d577517b711d31dd805c8f5a1dbd2de7fa9fcb8f
Alisson <rodrigues101112@gmail.com>
2025-01-22 17:59:50 -0300

    update files

--- routes/cards.js
+++ routes/cards.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import fs from "node:fs";
 import path from "node:path";
-import { v4 as uuidv4 } from "uuid";
 
 const router = Router();
 const __dirname = import.meta.dirname;
 let cards = [];
 const file = path.join(__dirname, "..", "data", "cards.json");
+
 fs.readFile(file, (error, data) => {
   if (error) {
     console.log(error);
@@ -15,20 +15,10 @@ fs.readFile(file, (error, data) => {
   }
   cards = JSON.parse(data);
 });
+
 router.get("/", (request, response) => {
-  if (cards.error) {
-    return response.status(404).json(cards);
-  }
-  return response.json(cards);
-});
-router.get("/:id", (request, response) => {
-  const { id } = request.params;
-  const card = cards.find((card) => card.id === id);
-  if (cards.error) {
-    return response.status(404).json(cards);
-  }
-  if (!card) {
-    return response.status(404).json({ message: `O id não foi encontrado !` });
+  if (!cards) {
+    return response.status(404).json({ message: `Cartões não encontrados !` });
   }
   return response.json(cards);
 });
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import fs from "node:fs";
 import path from "node:path";
-import { v4 as uuidv4 } from "uuid";
 
 const router = Router();
 const __dirname = import.meta.dirname;
 let users = [];
+
 const file = path.join(__dirname, "..", "data", "users.json");
 fs.readFile(file, (error, data) => {
   if (error) {
@@ -17,60 +17,22 @@ fs.readFile(file, (error, data) => {
 });
 
 router.get("/", (request, response) => {
-  if (users.error) {
-    return response.status(404).json(users);
+  if (!users) {
+    return response.status(404).json({ message: `Usuarios não encontrados !` });
   }
   return response.json(users);
 });
 
-router.get("/:id", (request, response) => {
-  const { id } = request.params;
-  const user = users.find((user) => user.id === id);
-  if (users.error) {
-    return response.status(404).json(users);
-  }
+router.get("/:_id", (request, response) => {
+  const { _id } = request.params;
+  const user = users.find((user) => user._id === _id);
+
   if (!user) {
-    return response.status(404).json({ message: `O id não foi encontrado !` });
+    return response
+      .status(404)
+      .json({ message: `O usuario com id ${id} não foi encontrado !` });
   }
-  return response.json(users);
+  return response.json(user);
 });
 
-router.post("/", (request, response) => {
-  if (users.error) {
-    return response.status(404).json(users);
-  }
-  const body = request.body;
-  const newUser = { id: uuidv4(), ...body };
-  users.push(newUser);
-  return response.json(users);
-});
-
-router.put("/:id", (request, response) => {
-  if (users.error) {
-    return response.status(404).json(users);
-  }
-  const { id } = request.params;
-  const user = users.find((user) => user.id === id);
-  if (!user) {
-    return response.status(404).json({ message: `O id não foi encontrado !` });
-  }
-  const body = request.body;
-  for (const key in body) {
-    user[key] = body[key];
-  }
-  return response.status(201).json(user);
-});
-
-router.delete("/:id", (request, response) => {
-  if (users.error) {
-    return response.status(404).json(users);
-  }
-  const { id } = request.params;
-  const user = users.find((user) => user.id === id);
-  if (!user) {
-    return response.status(404).json({ message: `O id não foi encontrado !` });
-  }
-  users.splice(users.indexOf(user), 1);
-  return response.status(204).json();
-});
 export { router };
